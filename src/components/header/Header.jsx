64208b045527a4b89a469291c04f6fdc60036b20
Arsalan <arsalan252009@gmail.com>
2026-09-01 22:40:39 +0500

    .

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../../assets/logo.png'
+import Logo from '../../asset/logo.png'
 import styles from './Header.module.css'
 
 function Header({HeaderButtonOnClick,HeaderButtonText}){
