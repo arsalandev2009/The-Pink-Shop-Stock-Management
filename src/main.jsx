da4d5c6e321b63447013ceb6dcf20e82962831e3
Arsalan <arsalan252009@gmail.com>
2026-09-01 22:21:19 +0500

    .

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'The Pink Shop',
+        short_name: 'The Pink Shop',
+        description: 'Stock management application',
+
+    theme_color: '#F62477',
+    background_color: '#FFF5F8',
+
+        display: 'standalone',
+        start_url: '/',
+
+        icons: [
+          {
+            src: '/logo.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logo.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
